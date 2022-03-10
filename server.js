3b8ba9162e2d92cb5b4a160bb8a44115f8765060
empipio <94937507+empipio@users.noreply.github.com>
2022-03-10 15:21:42 +0000

    changed path for landing page

--- server.js
+++ server.js
@@ -10,13 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+// app.get("/api/notes", function (req, res) {
+//   res.readFile(db.json);
+// });
 
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
 
